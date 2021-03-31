011bc9f04517a54a8f92abd0f317c2570ed6c21f
ABHIJEET KUMAR SINGH <1398abhisingh911@gmail.com>
2021-03-31 11:50:35 +0530

    P

---
+++ cgi-bin files/ansible.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import cgi
+import subprocess
+import os
+
+print("content-type: text/html")
+print()
+
+x=cgi.FieldStorage()
+y1=x.getvalue("linux1")
+y2=x.getvalue("linux2")
+
+
+if y1 is not None:
+  cmd="ansible-playbook cluster.yml"
+  y=subprocess.getoutput(cmd)
+  print(y)
+
+if y2 is not None:
+  cmd="ansible-playbook task11master.yml"
+  cmd2="ansible-playbook task11slave.yml"
+  y=subprocess.getoutput(cmd)
+  y2=subprocess.getoutput(cmd2)
+  print(y)
+  print(y2)
+
+
