361678367ae947162acacc419ebac3545d522dde
Abhijeet Singh <1398abhisingh911@gmail.com>
2021-04-01 14:52:28 +0530

    Update docker.py

--- cgi-bin files/docker.py
+++ cgi-bin files/docker.py
@@ -1,3 +1,17 @@
+
+
+Skip to content
+Using Gmail with screen readers
+Meet
+New meeting
+Join a meeting
+Hangouts
+
+Conversations
+7.02 GB of 15 GB used
+Terms · Privacy · Program Policies
+Last account activity: 0 minutes ago
+Open in 1 other location · Details
 #!/usr/bin/python3
 
 import cgi
@@ -26,6 +40,59 @@ y11=x.getvalue("dhvol")
 
 y12=x.getvalue("namecon")
 y13=x.getvalue("nameima")
+
+y14=x.getvalue("constop")
+y15=x.getvalue("conpull")
+y16=x.getvalue("version")
+y17=x.getvalue("constart")
+
+y18=x.getvalue("condel")
+y19=x.getvalue("versiondel")
+y20=x.getvalue("linux6")
+
+y21=x.getvalue("conname")
+y22=x.getvalue("tag")
+y23=x.getvalue("ver")
+
+y24=x.getvalue("conname2")
+y25=x.getvalue("ver2")
+
+y26=x.getvalue("conname3")
+
+y27=x.getvalue("log1")
+
+
+y28=x.getvalue("conname4")
+y29=x.getvalue("comma")
+
+if y28 is not None and y29 is not None:
+  cmd="sudo docker exec {} {}".format(y28,y29)
+  y=subprocess.getoutput(cmd)
+  print(y)
+
+
+
+if y27 is not None:
+  cmd="sudo docker logs {}".format(y27)
+  y=subprocess.getoutput(cmd)
+  print(y)
+  
+if y26 is not None:
+  cmd="sudo docker inspect {}".format(y26)
+  y=subprocess.getoutput(cmd)
+  print(y)
+
+if y21 is not None and y23 is not None:
+  cmd="sudo docker commit {} {}:{}".format(y21,y22,y23)
+  y=subprocess.getoutput(cmd)
+  print(y)
+
+if y24 is not None and y25 is not None:
+  cmd="sudo docker push {}:{}".format(y24,y25)
+  y=subprocess.getoutput(cmd)
+  print(y)
+
+
 
 if cmd is not None:
   cmd="sudo {}".format(cmd)
@@ -57,6 +124,11 @@ if y5 is not None:
   y=os.system(cmd)
   print(y)
 
+if y20 is not None:
+  cmd="sudo docker container rm -f $(docker container ls -a -q)"
+  y=subprocess.getoutput(cmd)
+  print(y)
+
 
 if y6 is not None and y7 is not None: 
   if y8 is not None and y9 is not None:
@@ -86,3 +158,29 @@ if y13 is not None:
   cmd="sudo docker image rm -f {}".format(y13)
   y=subprocess.getoutput(cmd)
   print(y)
+
+if y14 is not None:
+  cmd="sudo docker stop {}".format(y14)
+  y=subprocess.getoutput(cmd)
+  print(y)
+
+if y15 is not None and y16 is not None:
+   cmd="sudo docker pull {}:{}".format(y15,y16)
+   y=subprocess.getoutput(cmd)
+   print(y)
+
+
+if y17 is not None:
+  cmd="sudo docker start {}".format(y17)
+  y=subprocess.getoutput(cmd)
+  print(y)
+
+
+if y18 is not None and y19 is not None:
+  cmd="sudo docker image rmi {}:{} -f".format(y18,y19)
+  y=subprocess.getoutput(cmd)
+  print(y)
+
+
+
+
